c88fa85ad730ae363220574f714de7a749cefea6
Irina-AI-maker <you@example.com>
2026-03-08 11:19:23 +0600

    Изучила f-строки

---
+++ Fstroka.py
@@ -0,0 +1,8 @@
+user_name="irina"
+topic="Нейросети"
+text=f"Привет, {user_name.title()}! Сегодня мы изучаем {topic}"
+message=f"\t{text}. Поздравляю с праздником!"
+new_message=f"{text}. Поздравляю!"
+print(message)
+print(new_message)
+print(f'\t{new_message}')
