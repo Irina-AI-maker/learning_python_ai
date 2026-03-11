b45a8dde30f2cb649e3c5d01b671b3c2a2623ad4
Irina-AI-maker <you@example.com>
2026-03-11 18:23:50 +0600

    Завершила Главу 4: от циклов до идеального стиля PEP 8

---
+++ copylist.py
@@ -0,0 +1,8 @@
+my_ai_models=["gpt","claude","sora"]
+friens_models=my_ai_models[:]
+print(my_ai_models)
+print(friens_models)
+my_ai_models.append('gemini')
+friens_models.append("deepseek")
+print(my_ai_models)
+print(friens_models)
